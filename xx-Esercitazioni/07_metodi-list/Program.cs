9c425dee2d13df264bf1ca2f7b4d0cbf67e8c670
Gio-0326 <giojuv2003@gmail.com>
2024-10-18 11:09:42 +0200

    07 metodi list

---
+++ xx-Esercitazioni/07_metodi-list/Program.cs
@@ -0,0 +1,79 @@
+﻿// METODI LISTA
+/*
+i metodi disponibili per manipolare le liste sono:
+- Count
+- Add
+- AddRange
+- Clear
+- Contains
+- IndexOf
+- Insert
+- Remove
+- RemoveAt
+- Sort
+- ToArray
+- TrimExcess
+*/
+
+Console.Clear();
+
+// ESEMPIO DI METODO COUNT
+// restituisce il numero di elementi di una lista
+List<int> lista1 = new List<int> { 1, 2, 3, 4, 5 };
+Console.WriteLine(lista1.Count);
+
+// ESEMPIO DI METODO ADD
+// aggiunge un elemnto alla fine di una lista 
+List<int> lista2 = new List<int> { 1, 2, 3, 4, 5 };
+lista2.Add(6);
+Console.WriteLine(string.Join(", ", lista2));
+
+// ESEMPIO DI METODO ADDRANGE
+// aggiunge una collezione alla fine di una lista
+List<int> lista3 = new List<int> { 1, 2, 3, 4, 5 };
+List<int> lista4 = new List<int> { 6, 7, 8, 9, 10 };
+lista3.AddRange(lista4);
+
+// ESEMPIO DI METODO CLEAR
+// cancella gli elementi di una lista
+List<int> lista5 = new List<int>{ 1, 2, 3, 4, 5 };
+lista5.Clear();
+// posso cancellare un elemento specifico con lista5.Remove(3) che cancella il primo 3 trovarto
+lista5.Remove(3); // cancella il primo 3 trovato
+Console.WriteLine(string.Join(",", lista5));
+
+// ESEMPIO DI METODO CONTAINS
+// restituisce true se una lista contiene un elemento
+List<int> lista6 = new List<int> {1, 2, 3, 4, 5 };
+Console.WriteLine(lista6.Contains(3));
+
+// ESEMPIO DI METODO INDEXOF
+// restituisce l indice di un elemento di una lista 
+List<int> lista7 = new List<int> { 1, 2, 3, 4, 5 };
+Console.WriteLine(lista7.IndexOf(3));
+
+// ESEMPIO DI METODO REMOVE
+// cancella la prima occorenza di un elemento di una lista
+List<int> lista8 = new List<int> { 1, 2, 3, 4, 5 };
+lista8.Remove(3);
+Console.WriteLine(string.Join(",", lista8));
+
+// ESEMPIO DI METODO REMOVEAT
+
+List<int> lista9 = new List<int> { 1, 2, 3, 4, 5 };
+lista9.RemoveAt(2);
+Console.WriteLine(string.Join(",", lista9));
+
+// ESEMPIO DI METODO SORT
+// ordina gli elementi di una lista
+List<int> lista10 = new List<int> { 5, 3, 1, 4, 2 };
+lista10.Sort();
+Console.WriteLine(string.Join(",", lista10));
+
+// ESEMPIO DI METODO TOARRAY
+// restituisce un array a partire da una lista 
+List<int> lista11 = new List<int> { 1, 2, 3, 4, 5 };
+int[] array = lista11.ToArray();
+Console.WriteLine(array.Length);
+
+// ESEMPIIO DI METODO TRIMEXCESS
