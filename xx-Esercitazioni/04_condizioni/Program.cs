67bed91c4b1b16399baa4e18644899f4045b6e03
Gio-0326 <giojuv2003@gmail.com>
2024-10-18 10:05:06 +0200

    cicli

--- xx-Esercitazioni/04_condizioni/Program.cs
+++ xx-Esercitazioni/04_condizioni/Program.cs
@@ -43,3 +43,22 @@ else
     Console.WriteLine("x e minore di 5");
 }
 // else if va messo tra if e else perche se messo dopo else non verrebbe mai eseguito
+
+// ESEMPIO DI SWITCH
+// esegue un blocco di codice in base al valore di una variabile
+int y = 10;
+switch (y)
+{
+    case 5:
+    Console.WriteLine("y e uguale a 5");
+    break; // break serve per uscire dallo switch
+    case 10:
+    Console.WriteLine("y e uguale a 10");
+    break;
+    default:
+    Console.WriteLine("y non e ne 5 ne 10");
+    break;
+}
+// esempio di switch con stringhe
+
+// esempio di switch con bool
