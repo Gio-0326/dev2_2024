a49d1bab521f377d85c8994648bf1ac6e7d389a7
Gio-0326 <giojuv2003@gmail.com>
2025-02-11 16:58:05 +0100

    aggiornamento

--- dev2_2024/Assignement/02-sorteggio-partecipanti/obj/Debug/net8.0/02-sorteggio-partecipanti.AssemblyInfo.cs
+++ dev2_2024/Assignement/02-sorteggio-partecipanti/obj/Debug/net8.0/02-sorteggio-partecipanti.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02-sorteggio-partecipanti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358ccdfaf8a9a63e33dec76d786960cc832202c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208230d6aee81a2f744fedbfa5cd4d2964cfa68c")]
 [assembly: System.Reflection.AssemblyProductAttribute("02-sorteggio-partecipanti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02-sorteggio-partecipanti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
